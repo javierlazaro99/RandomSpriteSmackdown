cd7b45a6faac250caa882f74b34ce44b5fe375bb
javil <javier.lazaro@opendeusto.es>
2018-09-27 16:31:25 +0200

    Cmabio estructura Personaje, añadidas clases que heredan de personaje, personaje jugable y enemigo

--- RandomSpriteSmackdown/src/personaje/Personaje.java
+++ RandomSpriteSmackdown/src/personaje/Personaje.java
@@ -1,25 +1,17 @@
-package clases;
+package personaje;
 
-public class Personaje {
+import javax.swing.JFrame;
+
+public abstract class Personaje {
 	
-	private String nombre;
 	private int fuerza;
 	private int vida;
 	private int velocidad;
 	
-	public Personaje(String nombre, int fuerza, int vida, int velocidad) {
-		this.nombre = nombre;
+	public Personaje(int fuerza, int vida, int velocidad) {
 		this.fuerza = fuerza;
 		this.vida = vida;
 		this.velocidad = velocidad;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
 	}
 
 	public int getFuerza() {
@@ -48,14 +40,14 @@ public class Personaje {
 
 	@Override
 	public String toString() {
-		return "Personaje [nombre=" + nombre + ", fuerza=" + fuerza + ", vida=" + vida + ", velocidad=" + velocidad
+		return "Personaje [fuerza=" + fuerza + ", vida=" + vida + ", velocidad=" + velocidad
 				+ "]";
 	}
 	
 	/** Método para calcular un golpe en la lógica interna del programa
 	 * @param enemigo personaje que recibe el golpe
 	 */
-	public void DarGolpe(Personaje enemigo) {
-		enemigo.setVida((int) (enemigo.getVida() - this.getFuerza() * 0.5));
-	}
+	public abstract void DarGolpe(Personaje enemigo);
+	
+	public abstract void Moverse(JFrame stage, int movX, int movY);
 }
