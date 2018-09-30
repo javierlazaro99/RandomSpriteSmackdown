f045fb8c45acd236d34f4c8e5856d38bb4c20a57
GUILLERMO <GUILLERMO@WIN81PRO>
2018-09-30 14:38:19 +0200

    No se como hacer el hueco vacio

--- RandomSpriteSmackdown/src/Ventanas/VentanaPrincipal.java
+++ RandomSpriteSmackdown/src/Ventanas/VentanaPrincipal.java
@@ -1,85 +1,116 @@
 package Ventanas;
 
+
+
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+
 
 import Personalizados.Fondo;
 import Personalizados.FondoSwing;
+import Personalizados.JLabelGraficoAjustado;
+import Personalizados.JPanelBackground;
 
 
 
 
 public class VentanaPrincipal extends JFrame{
-
+	JPanel panel;
+	JButton botonHoistoria;
+	JButton botonPractica;
+	JButton boton1VS1;
+	JButton botonAyuda;
+	
+	boolean sigue = true;
 	public VentanaPrincipal() {
+		// TODO Auto-generated constructor stub
+		//Settings
+		setTitle("RandomSprite Smackdown");
+		setSize(1024, 768);
+		setLocationRelativeTo(null);
 		
-		//Iniciacion de ventana
-		//(new ImageIcon("src\\Fondo.gif"));
-		
-		
-		setTitle("RandomSprite Smackdown");//titulo del juego
-		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );//Operacion de cierre
-		setSize(900, 700);//parametro de medidas de la ventana
-		setLocationRelativeTo(null);//Centrado
-		setLayout(new GridLayout());
-		
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setLayout(new FlowLayout());
 		//Contenedores
 		
-		//JLabel fondo = new JLabel(new ImageIcon("src\\Fondo.gif"));
-		JPanel  paneliz = new JPanel();
-		//Panel  botones principales
-			JPanel panelinterior = new JPanel();
-			JButton botonHistoria = new JButton(new ImageIcon("src\\Historia.gif"));
-			JButton botonPractica = new JButton(new ImageIcon("src\\Practica.gif"));
-			JButton boton1VS1 = new JButton(new ImageIcon("src\\1VS1.gif"));
-			JButton botonAyuda = new JButton(new ImageIcon("src\\Ayuda.PNG"));
-		JPanel panelde = new JPanel();//Panel de titulo
-			JLabel labelTitulo = new JLabel();
+		JPanelBackground fondo = new JPanelBackground("src/Fondo.gif");
+		
+		
+		
+		JLabelGraficoAjustado titulo = new JLabelGraficoAjustado("", 750, 137);
+		JPanel paneliz= new  JPanel();
+		JPanel panelde = new JPanel();
+		JPanel panelinterior = new JPanel();
+		
+		
+		JButton botonHistoria= new JButton(new ImageIcon("src/Historia1.gif"));
+		JButton botonPractica = new JButton(new ImageIcon("src/Practica1.gif"));
+		JButton boton1VS1= new JButton(new ImageIcon("src/1VS11.gif"));
+		JButton botonAyuda = new JButton(new ImageIcon("src/Ayuda1.gif"));
+		
 		//Modificaciones
-			panelinterior.setLayout(new BoxLayout(panelinterior,BoxLayout.Y_AXIS));
-			
-			
-			paneliz.setOpaque(false);
-			panelde.setOpaque(false);
-			panelinterior.setOpaque(false);
-			boton1VS1.setOpaque(false);
-			botonAyuda.setOpaque(false);
-			botonHistoria.setOpaque(false);
-			botonPractica.setOpaque(false);
-			botonHistoria.setSize(100, 100);
-			botonPractica.setSize(100, 100);
-			botonAyuda.setSize(100, 100);
-			boton1VS1.setSize(100, 100);
-			//fondo.setOpaque(false);
-			Dimension fondosize =getSize();
-			//fondo.setSize(fondosize);
-			
-			
-		//Añadir los contenedores
-		//add(fondo);
-		add(paneliz);
-		add(panelde);
 		
 		
+		titulo.setImagen("src/Titulo.PNG");
+		
+		
+		this.setLayout(new BorderLayout());
+		panelde.setOpaque(false);
+		paneliz.setOpaque(false);
+		panelinterior.setOpaque(false);
+		panelinterior.setLayout(new BoxLayout(panelinterior, BoxLayout.Y_AXIS));
+		titulo.setOpaque(false);
+		titulo.setBorder(null);
+		fondo.setLayout(new BoxLayout(fondo,BoxLayout.X_AXIS));
+		botonHistoria.setSize(200,200);	
+		botonHistoria.setOpaque(false);
+		botonHistoria.setContentAreaFilled(false);
+		botonHistoria.setBorder(null);
+		botonPractica.setOpaque(false);
+		botonPractica.setContentAreaFilled(false);
+		botonPractica.setBorder(null);
+		boton1VS1.setOpaque(false);
+		boton1VS1.setContentAreaFilled(false);
+		
+		boton1VS1.setBorder(null);
+		botonAyuda.setOpaque(false);
+		botonAyuda.setContentAreaFilled(false);
+		botonAyuda.setBorder(null);
+		
+		
+		
+		
+		
+		
+		//Meter contenedores
+		this.add(fondo, BorderLayout.CENTER);
+		
+		
+		fondo.add(paneliz);
+		fondo.add(panelde);
+		panelde.add(titulo,BorderLayout.NORTH);
 		paneliz.add(panelinterior,BorderLayout.CENTER);
+		
 		panelinterior.add(botonHistoria);
 		panelinterior.add(botonPractica);
 		panelinterior.add(boton1VS1);
 		panelinterior.add(botonAyuda);
-		panelde.add(labelTitulo,BorderLayout.NORTH);
 		
 	}
 	
-	
-
-
-
-
-public static void main(String[] args) {
+	public static void main(String[] args) {
 		VentanaPrincipal ventana = new VentanaPrincipal();
 		ventana.setVisible(true);
 	}
