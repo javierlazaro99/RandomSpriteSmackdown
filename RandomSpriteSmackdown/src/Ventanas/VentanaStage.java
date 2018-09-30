769fae73cce519a024c1a5589aa7d8a78230bad5
javil <javier.lazaro@opendeusto.es>
2018-09-30 13:47:12 +0200

    Ventana de stage empezada

--- RandomSpriteSmackdown/src/Ventanas/VentanaStage.java
+++ RandomSpriteSmackdown/src/Ventanas/VentanaStage.java
@@ -1,10 +1,17 @@
 package Ventanas;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
 import Personalizados.FondoSwing;
 import Personalizados.JPanelBackground;
@@ -15,13 +22,67 @@ public class VentanaStage extends JFrame {
 
 	public VentanaStage() {
 		
-		setSize(500, 500);
+		setSize(1920, 1080);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
-		JPanelBackground jpb = new JPanelBackground("src/Fondo.gif");
+		JPanelBackground jpBackground = new JPanelBackground("src/Stage1.gif");
+		JPanel pNorte = new JPanel(new GridLayout(2, 1));
+			JPanel pNs = new JPanel();
+			JPanel pNi = new JPanel(new GridLayout(1, 3));
+			JPanel pN1 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+			JPanel pN2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+			JPanel pN3 = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		
 		this.setLayout(new BorderLayout());
-		this.add(jpb, BorderLayout.CENTER);
+		this.add(jpBackground, BorderLayout.CENTER);
+		
+		pNorte.setPreferredSize(new Dimension(4000, 150));
+		
+		JProgressBar jpbVida1 = new JProgressBar();
+		JProgressBar jpbVida2 = new JProgressBar();
+		JLabel lTiempo = new JLabel("60");
+		
+		lTiempo.setFont(new Font("", Font.PLAIN, 40));
+		
+		jpbVida1.setPreferredSize(new Dimension(500, 60));
+		jpbVida2.setPreferredSize(new Dimension(500, 60));
+		lTiempo.setPreferredSize(new Dimension(50, 50));
+		
+		jpbVida1.setValue(500);
+		jpbVida2.setValue(500);
+		jpbVida1.setBorderPainted(false);
+		jpbVida2.setBorderPainted(false);
+		jpbVida1.setOpaque(false);
+		jpbVida2.setOpaque(false);
+		jpbVida1.setForeground(Color.RED);
+		jpbVida2.setForeground(Color.RED);
+		
+//		pNorte.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+//		pN1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+		
+		jpBackground.setLayout(new BorderLayout());
+		jpBackground.add(pNorte, BorderLayout.NORTH);
+		pNorte.setOpaque(false);
+		pNs.setOpaque(false);
+		pNi.setOpaque(false);
+		pN1.setOpaque(false);
+		pN2.setOpaque(false);
+		pN3.setOpaque(false);
+		
+		pNorte.add(pNs);
+		pNorte.add(pNi);
+		
+		pNi.add(pN1); 
+		pNi.add(pN2); 
+		pNi.add(pN3);
+		
+		pN1.add(jpbVida1);
+		pN2.add(lTiempo);
+		pN3.add(jpbVida2);
+		
+		
+		
 		
 	}
 	
---
+++ RandomSpriteSmackdown/src/control/ControlStage.java
@@ -0,0 +1,15 @@
+package control;
+
+import personaje.Personaje;
+
+public class ControlStage {
+	
+	private Personaje personaje1;
+	private Personaje personaje2;
+	
+	public ControlStage(Personaje p1, Personaje p2) {
+		this.personaje1 = p1;
+		this.personaje2 = p2;
+	}
+
+}
