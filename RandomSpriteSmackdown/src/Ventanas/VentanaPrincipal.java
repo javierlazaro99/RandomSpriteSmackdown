52c3fac1d0dd61698520634455fe86fd18472aa5
GUILLERMO <GUILLERMO@WIN81PRO>
2018-09-30 16:20:26 +0200

    Organizacion de botones

--- RandomSpriteSmackdown/src/Ventanas/VentanaPrincipal.java
+++ RandomSpriteSmackdown/src/Ventanas/VentanaPrincipal.java
@@ -10,6 +10,10 @@ import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
@@ -44,70 +48,99 @@ public class VentanaPrincipal extends JFrame{
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setLayout(new FlowLayout());
 		//Contenedores
-		
-		JPanelBackground fondo = new JPanelBackground("src/Fondo.gif");
-		
-		
-		
+		JPanelBackground sticman = new JPanelBackground("src/Stickman1.gif");
+		JPanelBackground fondo = new JPanelBackground("src/Fondo.gif");//Fondo
 		JLabelGraficoAjustado titulo = new JLabelGraficoAjustado("", 650, 137);
 		JPanel paneliz= new  JPanel();
 		JPanel panelde = new JPanel();
 		JPanel panelinterior = new JPanel();
-		
-		
 		JButton botonHistoria= new JButton(new ImageIcon("src/Historia1.gif"));
 		JButton botonPractica = new JButton(new ImageIcon("src/Practica1.gif"));
 		JButton boton1VS1= new JButton(new ImageIcon("src/1VS11.gif"));
 		JButton botonAyuda = new JButton(new ImageIcon("src/Ayuda1.gif"));
 		
 		//Modificaciones
-		
-		
-		titulo.setImagen("src/Titulo.PNG");
-		
-		
+		//LAYAOUT
 		this.setLayout(new BorderLayout());
-		panelde.setOpaque(false);
-		paneliz.setOpaque(false);
-		paneliz.setLayout(new GridLayout(0,1));
-		panelinterior.setOpaque(false);
-		
-		titulo.setOpaque(false);
-		titulo.setBorder(null);
+		paneliz.setLayout(new GridLayout(0,1,0,2));
 		fondo.setLayout(new BoxLayout(fondo,BoxLayout.X_AXIS));
 		
+		//Añadir imagen
+		titulo.setImagen("src/Titulo.PNG");
+		//Opaques
+		titulo.setOpaque(false);
+		panelde.setOpaque(false);
+		paneliz.setOpaque(false);
 		botonHistoria.setOpaque(false);
-		botonHistoria.setContentAreaFilled(false);
-		botonHistoria.setBorder(null);
 		botonPractica.setOpaque(false);
-		botonPractica.setContentAreaFilled(false);
-		botonPractica.setBorder(null);
-		boton1VS1.setOpaque(false);
-		boton1VS1.setContentAreaFilled(false);
-		
-		boton1VS1.setBorder(null);
 		botonAyuda.setOpaque(false);
-		botonAyuda.setContentAreaFilled(false);
+		panelinterior.setOpaque(false);
+		boton1VS1.setOpaque(false);
+		sticman.setOpaque(false);
+		//Border
+		titulo.setBorder(null);
+		botonHistoria.setBorder(null);
+		botonPractica.setBorder(null);
+		boton1VS1.setBorder(null);
 		botonAyuda.setBorder(null);
-		
-		
-		
-		
-		
+		//Contenedor a null
+		botonHistoria.setContentAreaFilled(false);
+		botonPractica.setContentAreaFilled(false);
+		boton1VS1.setContentAreaFilled(false);
+		botonAyuda.setContentAreaFilled(false);
 		
 		//Meter contenedores
 		this.add(fondo, BorderLayout.CENTER);
-		
-		
+		//Estructura principal
 		fondo.add(paneliz);
 		fondo.add(panelde);
 		panelde.add(titulo,BorderLayout.NORTH);
+		panelde.add(sticman,BorderLayout.CENTER);
 		paneliz.add(panelinterior);
-		
 		paneliz.add(botonHistoria);
 		paneliz.add(botonPractica);
 		paneliz.add(boton1VS1);
 		paneliz.add(botonAyuda);
+		
+		//Eventos
+		
+		//Boton historia activa pagina principal
+		botonHistoria.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		//Boton Practica activa stage personalizado
+		botonPractica.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		//Boton 1VS1 
+		boton1VS1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		//Boton Ayuda
+		botonAyuda.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 		
 	}
 	
