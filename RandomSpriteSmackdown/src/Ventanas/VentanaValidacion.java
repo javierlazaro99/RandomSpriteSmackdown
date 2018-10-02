1013c9ecacbc8ab66108c1e9c3bef278ead21cf1
GUILLERMO <GUILLERMO@WIN81PRO>
2018-10-02 19:04:41 +0200

    DATOS A GUARDAR FALTAN

---
+++ RandomSpriteSmackdown/src/Ventanas/VentanaValidacion.java
@@ -0,0 +1,11 @@
+package Ventanas;
+
+import javax.swing.JFrame;
+
+public class VentanaValidacion extends JFrame {
+	
+	public VentanaValidacion() {
+		// TODO Auto-generated constructor stub
+		setTitle("Panel login/register");
+	}
+}
