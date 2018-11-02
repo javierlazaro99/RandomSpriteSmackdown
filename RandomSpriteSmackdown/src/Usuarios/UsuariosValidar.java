05a8112033937df9db9efa39a5e8b8eddfe024e8
guvil <guvil@DESKTOP-F1986US>
2018-11-02 13:45:41 +0100

    Control de usuarios hecho a bd y logger activados

--- RandomSpriteSmackdown/src/Usuarios/UsuariosValidar.java
+++ RandomSpriteSmackdown/src/Usuarios/UsuariosValidar.java
@@ -7,16 +7,28 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+import Ventanas.VentanaValidacionUsuarios;
+
 public class UsuariosValidar {
 	private String nombre;
 	private String password;
+	private Connection con =null;
+	private Statement s;
 	private String path = "Usuario.txt";
+	private Logger logger=VentanaValidacionUsuarios.getLogger();
 	ArrayList<String> datos = new ArrayList<>();
 	public UsuariosValidar(String nombre,String passwod) {
 		// TODO Auto-generated constructor stub
@@ -42,69 +54,50 @@ public class UsuariosValidar {
 	}
 
 	public void guardar(UsuariosValidar usuario) {
-		
 		try {
-		
-		File fi = new File(path);
-		FileWriter fw	 = new FileWriter(fi);
-		BufferedWriter bw = new BufferedWriter(fw);
-		bw.write("Usuario:"+usuario.nombre+";"+"Password:"+usuario.password);
-		bw.newLine();
-		bw.flush();
-		bw.close();
-		} catch (IOException e) {
+			con= DriverManager.getConnection( "jdbc:sqlite:randomspritesmackdown.db") ;
+			s=con.createStatement();
+			String query= "INSERT INTO USUARIO VALUES('"+usuario.getNombre()+","+usuario.getPassword()+")";
+			s.executeUpdate(query);
+			logger.log(Level.INFO, "Usuario guardado correctamente en la BD");
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			logger.log(Level.SEVERE, "Error usuario no se ha guardado");
 		}
 		
+	
 	}
 	public UsuariosValidar leer(JTextField usuarioTexto,JPasswordField passwordTexto) {
 		
 		try {
-			File fi = new File(path);
-			FileReader fr = new FileReader(fi);
-			BufferedReader br = new BufferedReader(fr);
-			String line=br.readLine();
-			while(line!= null) {
-				String[] division=line.split(";");
-				
-				for(int a = 0;a<division.length;a++) {
-					String[] usuarioleido= division[0].split(":");
-					
-					datos.add(usuarioleido[1]);
-					//Falta lo de los espacios
-				}
-				if(validacion(datos, usuarioTexto, passwordTexto)) {
-					UsuariosValidar usuario = new UsuariosValidar(usuarioTexto.getText(), passwordTexto.getText());
-					br.close();
+			con= DriverManager.getConnection( "jdbc:sqlite:randomspritesmackdown.db") ;
+			s=con.createStatement();
+			String query= "SELECT NICK,PASSWORD FROM USUARIO";
+			ResultSet str =s.executeQuery(query);
+			while(str.next()) {
+				String nick=str.getString("NICK");
+				String password = str.getString("PASSWORD");
+				if(usuarioTexto.getText().equals(nick) && passwordTexto.getText().equals(password)) {
+					UsuariosValidar usuario = new UsuariosValidar(nick, password);
+					logger.log(Level.INFO, "Usuario cargado");
 					return usuario;
-				}else {
-					datos.removeAll(datos);
 				}
-				
-				
 			}
-			JOptionPane.showMessageDialog(null, "No has introducido los datos correctamente o el usuario no existe");	
-		return null;
-		} catch (FileNotFoundException e) {
+			logger.log(Level.INFO, "Usuario no existia");
+			return null;
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.log(Level.SEVERE, "Usuario cargado erroneamente");
 		}
 		return null;
 		
 		
 	}
-	public boolean validacion(ArrayList<String> datos,JTextField usuarioTexto,JPasswordField passwordTexto) {
-		if(datos.get(0).equals(usuarioTexto.getText()) && datos.get(1).equals(passwordTexto.getPassword()) ) {
-			return true;
-		}else {
-			return false;
-		}
+	
 		
-	}
+	
 	//Falta por hacer 
 	public void guardarDatos() {
 		 
