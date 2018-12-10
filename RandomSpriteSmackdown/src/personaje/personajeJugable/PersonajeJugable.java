08704dd7bbbe265e18eccba02d42e73308446b03
javil <javier.lazaro@opendeusto.es>
2018-12-10 11:32:54 +0100

    Golpe sin que se corte + prueba de quitar daño al enemigo

--- RandomSpriteSmackdown/src/personaje/personajeJugable/PersonajeJugable.java
+++ RandomSpriteSmackdown/src/personaje/personajeJugable/PersonajeJugable.java
@@ -106,6 +106,7 @@ public class PersonajeJugable extends Personaje {
 	@Override
 	public void DarGolpe(Personaje enemigo) {
 		double distancia = Math.sqrt(Math.pow(enemigo.getPosicion().getX() - this.getPosicion().getX(), 2));
+		
 		if(distancia < 5) {
 			//Se ejecuta el pegado
 			enemigo.setVida(enemigo.getVida() - 0.1 * this.getFuerza());
