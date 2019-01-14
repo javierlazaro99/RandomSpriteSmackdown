6942a3dd7465ed86fb8c9c6ac7edb256dd142b3a
Javi <javier.lazaro@opendeusto.es>
2019-01-14 12:50:39 +0100

    Creada la clase mouse adapters que tiene como clases internas los distintos adapters que utilizamos para los botones

---
+++ RandomSpriteSmackdown/src/control/MouseAdapters.java
@@ -0,0 +1,93 @@
+package control;
+
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+/**
+ * Clase que guarda las distintas subclases de mouse adapters
+ * personalizados que utilizan los botones
+ * @author pc
+ *
+ */
+public class MouseAdapters {
+	
+	public MouseAdapters() {
+	}
+	
+	/**
+	 * Adaptador para los Botones de subida de nivel de la ventana mejoras
+	 * @author pc
+	 *
+	 */
+	public static class MouseAdapterSubirNivel extends MouseAdapter{
+		
+		private JButton boton;
+		
+		public MouseAdapterSubirNivel(JButton b) {
+			this.boton = b;
+		}
+		
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			boton.setBorderPainted(true);
+			boton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));	
+			boton.setContentAreaFilled(true);
+			boton.setBackground(Color.orange);
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			boton.setBorder(null);
+			boton.setContentAreaFilled(false);
+		}
+	}
+	
+	public static class MouseAdapterBotonesVentanaPrincipal extends MouseAdapter{
+		
+		private JButton boton;
+		
+		public MouseAdapterBotonesVentanaPrincipal(JButton b) {
+			this.boton = b;
+		}
+		
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			boton.setBorderPainted(true);
+			boton.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));	
+			boton.setContentAreaFilled(true);
+			
+			float[] f = Color.RGBtoHSB(64, 64, 63, null);
+			boton.setBackground(Color.getHSBColor(f[0], f[1], f[2]));
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			boton.setBorder(null);
+			boton.setContentAreaFilled(false);
+		}
+	}
+	
+	public static class MouseAdapterBotonesMenus extends MouseAdapter{
+		
+		private JButton boton;
+		
+		public MouseAdapterBotonesMenus(JButton b) {
+			this.boton = b;
+		}
+		
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			float[] f = Color.RGBtoHSB(78, 42, 8, null);
+			boton.setBackground(Color.getHSBColor(f[0], f[1], f [2]));
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			boton.setBackground(Color.black);
+		}
+	}
+}
